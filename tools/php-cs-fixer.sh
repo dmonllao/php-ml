c775baf6eb9bf2d81fbda982d5972ff560175099
David Monllao <david.monllao@gmail.com>
2017-02-02 11:40:03 +0100

    Execute coding style fixer in travis
    Will fail the build if there is something to fix.

--- tools/php-cs-fixer.sh
+++ tools/php-cs-fixer.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+
+exitcode=0
+
 echo "Fixing src/ folder"
 php-cs-fixer fix src/
 
+csexitcode="$?"
+if [ "$csexitcode" != "0" ]; then
+    exitcode=$csexitcode
+fi
+
 echo "Fixing tests/ folder"
 php-cs-fixer fix tests/
+
+csexitcode="$?"
+if [ "$csexitcode" != "0" ]; then
+    # Overwrite the previous exitcode yes, there are worst things in life.
+    exitcode=$csexitcode
+fi
+
+# Forward php-cs-fixer exit codes.
+exit $exitcode
