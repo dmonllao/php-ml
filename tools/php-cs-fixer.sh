af2722a85b12f517b19dd7d1239669c8541da2a7
David Monllao <david.monllao@gmail.com>
2017-02-02 11:40:03 +0100

    Execute coding style fixer in travis
    Will fail the build if there is something to fix.

--- tools/php-cs-fixer.sh
+++ tools/php-cs-fixer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+exitcode=0
+
 echo "Fixing src/ folder"
 php-cs-fixer fix src/
+if [ "$?" -ne "0" ]; then
+    exitcode=1
+fi
 
 echo "Fixing tests/ folder"
 php-cs-fixer fix tests/
+if [ "$?" -ne "0" ]; then
+    exitcode=1
+fi
+
+exit $exitcode
