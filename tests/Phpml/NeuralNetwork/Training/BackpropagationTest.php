eeba8522c9ad9cb4c3020e815065e70b7094e96d
David Monllao <davidm@moodle.com>
2017-05-15 15:42:38 +0800

    MLPClassifier fails with multiple hidden layers or without $desiredError

--- tests/Phpml/NeuralNetwork/Training/BackpropagationTest.php
+++ tests/Phpml/NeuralNetwork/Training/BackpropagationTest.php
@@ -27,4 +27,45 @@ class BackpropagationTest extends TestCase
         $this->assertEquals(1, $network->setInput([1, 0])->getOutput()[0], '', $desiredError);
         $this->assertEquals(1, $network->setInput([0, 1])->getOutput()[0], '', $desiredError);
     }
+
+    public function testBackpropagationForXORLearningNoDesiredError()
+    {
+        // Without desiredError
+        $network = new MultilayerPerceptron([2, 2, 1]);
+        $training = new Backpropagation($network);
+
+        $training->train(
+            [[1, 0], [0, 1], [1, 1], [0, 0]],
+            [[1], [1], [0], [0]],
+            $desiredError = 0,
+            40000
+        );
+
+        $output = $network->setInput([1, 1])->getOutput()[0];
+        $this->assertLessThan(0.3, $output, '', $desiredError);
+        $output = $network->setInput([0, 0])->getOutput()[0];
+        $this->assertLessThan(0.3, $output, '', $desiredError);
+        $output = $network->setInput([1, 0])->getOutput()[0];
+        $this->assertGreaterThan(0.7, $output, '', $desiredError);
+        $output = $network->setInput([0, 1])->getOutput()[0];
+        $this->assertGreaterThan(0.7, $output, '', $desiredError);
+    }
+
+    public function testBackpropagationForXORLearningMultiLayer()
+    {
+        $network = new MultilayerPerceptron([2, 2, 2, 1]);
+        $training = new Backpropagation($network);
+
+        $training->train(
+            [[1, 0], [0, 1], [1, 1], [0, 0]],
+            [[1], [1], [0], [0]],
+            $desiredError = 0.3,
+            40000
+        );
+
+        $this->assertEquals(0, $network->setInput([1, 1])->getOutput()[0], '', $desiredError);
+        $this->assertEquals(0, $network->setInput([0, 0])->getOutput()[0], '', $desiredError);
+        $this->assertEquals(1, $network->setInput([1, 0])->getOutput()[0], '', $desiredError);
+        $this->assertEquals(1, $network->setInput([0, 1])->getOutput()[0], '', $desiredError);
+    }
 }
